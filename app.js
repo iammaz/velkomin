f4599034b360c6418bcb5d1c3490745329aead03
Magnús Blöndal <magnusblondal@Daddyo.local>
2015-05-06 22:29:49 +0000

    layout og login gluggi

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// routes
+var routes = require('./routes/index'),
+    users = require('./routes/users');
 
 var app = express();
 
@@ -14,12 +15,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon())
+   .use(logger('dev'))
+   .use(bodyParser.json())
+   .use(bodyParser.urlencoded())
+   .use(cookieParser())
+   .use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/login',function (req, res) {
+  res.render('login');
+});
+
+router.post('/login', function (req, res) {
+  console.log('inn');
+  res.redirect('/');
+})
+
+router.get('/logout',function (req, res) {
+  res.redirect('/');
+});
+
 module.exports = router;
